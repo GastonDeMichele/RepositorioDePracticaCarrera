07d2f11a27ca9cd93e72c9f067b5cfec06a4ab55
GastonDeMichele <gaston_dm_22@hotmail.com>
2024-05-01 13:04:19 -0300

    starting

---
+++ cursos/CFS/clase_2/ejerciciosVariables.ts
@@ -0,0 +1,53 @@
+//variables reutilizables para todos los ejercicios
+let moverAdelante: string = "mover hacia adelante";
+let girarIzquierda: string = "girar hacia la izquierda"
+let girarDerecha: string = "girar hacia la derecha"
+let removerPila: string = "remover pila"
+let rellenar: string = "rellenar"
+
+
+//granjera 1
+console.log(moverAdelante);
+console.log(removerPila);
+console.log(moverAdelante);
+console.log(removerPila);
+console.log(moverAdelante);
+console.log(removerPila);
+console.log(moverAdelante);
+console.log(removerPila);
+console.log(girarIzquierda);
+console.log(moverAdelante);
+console.log(removerPila);
+console.log(moverAdelante);
+console.log(removerPila);
+console.log(moverAdelante);
+console.log(removerPila);
+console.log(moverAdelante);
+console.log(removerPila);
+console.log(moverAdelante);
+console.log(removerPila);
+
+
+//Granjera 2
+console.log(``);
+console.log("mover hacia adelante");
+console.log("llenar");
+console.log("mover hacia adelante");
+console.log("mover hacia adelante");
+console.log("mover hacia adelante");
+console.log("llenar");
+console.log("mover hacia adelante");
+console.log("remover");
+console.log("mover hacia adelante");
+console.log("remover");
+console.log("mover hacia adelante");
+
+//Granjera 3
+console.log("girar a la izquierda");
+console.log("mover hacia adelante");
+console.log("girar a la derecha");
+console.log("mover hacia adelante");
+console.log("mover hacia adelante");
+console.log("girar a la derecha");
+console.log("mover hacia adelante");
+console.log("remover");
