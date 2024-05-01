660dc1ade3e433742489052ca70b9373fb6977a4
GastonDeMichele <gaston_dm_22@hotmail.com>
2024-05-01 17:56:58 -0300

    starting

--- cursos/CFS/clase_2/variablesEjercicios.ts
+++ cursos/CFS/clase_2/variablesEjercicios.ts
@@ -1,11 +1,17 @@
 //consigna 1 : Modificar el ejemplo de secuencia.
+console.log("//consigna 1 : Modificar el ejemplo de secuencia.")
+
 
 let moverAdelante: string = "mover hacia adelante";
 let girarIzquierda: string = "girar hacia la izquierda";
 let girarDerecha: string = "girar hacia la derecha";
 let removerPila: string = "remover pila";
 let rellenar: string = "rellenar";
-//granjera 1
+
+
+
+console.log("Granjera 1");
+console.log();
 console.log(moverAdelante);
 console.log(removerPila);
 console.log(moverAdelante);
@@ -25,9 +31,10 @@ console.log(moverAdelante);
 console.log(removerPila);
 console.log(moverAdelante);
 console.log(removerPila);
-
-
 //Granjera 2
+console.log();
+console.log("Granjera 2");
+
 console.log(moverAdelante);
 console.log(rellenar);
 console.log(moverAdelante);
@@ -39,9 +46,10 @@ console.log(removerPila);
 console.log(moverAdelante);
 console.log(removerPila);
 console.log(moverAdelante);
-
-
 //Granjera 3
+console.log();
+console.log("Granjera 3");
+
 console.log(girarIzquierda);
 console.log(moverAdelante);
 console.log(girarDerecha);
@@ -52,6 +60,8 @@ console.log(moverAdelante);
 console.log(removerPila);
 
 // Consigna 2: Modificar el ejemplo de base por altura.
+console.log();
+console.log("Consigna 2: Modificar el ejemplo de base por altura.");
 
 let base: number = 5;
 let altura: number = 10;
