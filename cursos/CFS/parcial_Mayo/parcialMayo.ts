635a8141ab1488c66865ec38a1ca75aa0d0ed96d
GastonDeMichele <gaston_dm_22@hotmail.com>
2024-06-03 14:51:55 -0300

    starting

--- cursos/CFS/parcial_Mayo/parcialMayo.ts
+++ cursos/CFS/parcial_Mayo/parcialMayo.ts
@@ -4,25 +4,25 @@ cuente la cantidad de a, e y o que hay en esa cadena y almacene esas cantidades 
 //ingreso cadena
 const cadenaTexto: string = "Aquellos que no pueden recordar el pasado estan condenados a repetirlo";
 //funcion para filtrar las vocales
-function filtroVocales(cadena: string):number[]{
-//declaro variables 
+function filtroVocales(cadena: string): number[] {
+    //declaro variables 
     let sumaA = 0;
-let sumaE = 0;
-let sumaO = 0;
-//inicio for para recorrer la cadena
-for (let i = 0; i < cadena.length; i++) {
-    let posicion = cadena[i];
-    //si la cadena cumple la condicion suma en uno la variable.
-    if(posicion=== "a" || posicion === "A"){
-        sumaA++;
-    } else if(posicion=== "e" || posicion === "E"){
-        sumaE++;
-    } else if(posicion=== "o" || posicion === "O"){
-        sumaO++;
-    }     
+    let sumaE = 0;
+    let sumaO = 0;
+    //inicio for para recorrer la cadena
+    for (let i = 0; i < cadena.length; i++) {
+        let posicion = cadena[i];
+        //si la cadena cumple la condicion suma en uno la variable.
+        if (posicion === "a" || posicion === "A") {
+            sumaA++;
+        } else if (posicion === "e" || posicion === "E") {
+            sumaE++;
+        } else if (posicion === "o" || posicion === "O") {
+            sumaO++;
+        }
     }
     //retorno los valores de las variables que suman si se cumple la condicion
-    return[sumaA,sumaE,sumaO]
+    return [sumaA, sumaE, sumaO]
 }
 
 const resultado = filtroVocales(cadenaTexto)
@@ -38,11 +38,11 @@ e = 10
 o = 7
 */
 
-/*
+
 
 function sumarArreglo(arreglo: number[]): number {
     let suma: number = 0;
-    for ( let i = 0; i < arreglo.length; i++) {
+    for (let i = 0; i < arreglo.length; i++) {
         suma += arreglo[i];
     }
     return suma;
@@ -62,10 +62,10 @@ function establecerColorPorNota(valor: number): string {
     if (valor >= 0 && valor < 7) {
         return "rojo";
     }
-    else if ( valor >=7 && valor <= 10) {
+    else if (valor >= 7 && valor <= 10) {
         return "verde";
     }
-     return "gris";
+    return "gris";
 }
 
 // Prueba de la función
@@ -76,11 +76,11 @@ console.log(establecerColorPorNota(11)); //  gris
 
 
 
-function obtenerPromedio(arr:number[]): number{    
+function obtenerPromedio(arr: number[]): number {
     let suma: number = sumarArreglo(arr);
     let promedio: number = suma / arr.length;
     console.log(promedio);
     return promedio
 }
 
-let variable:string;*/
+let variable: string;
